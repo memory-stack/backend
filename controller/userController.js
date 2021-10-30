d5fe9788d5fce998cf25c22f084e0c7c74c76d67
himanshumishra508 <mishrahimanshu.hmi@gmail.com>
2021-10-30 18:47:20 +0530

    Get user detail api

--- controller/userController.js
+++ controller/userController.js
@@ -1,225 +1,244 @@
-const User = require('../models/user.js');
-const nodemailer = require('nodemailer');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const User = require("../models/user.js");
+const nodemailer = require("nodemailer");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 // function for sending email using NodeMail
 const sendEmail = (email, authToken) => {
+  const gmail_email = process.env.GMAIL_EMAIL;
+  const gmail_password = process.env.GMAIL_PASSWORD;
+  var transport = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    auth: {
+      user: gmail_email,
+      pass: gmail_password,
+    },
+  });
 
-    const gmail_email = process.env.GMAIL_EMAIL;
-    const gmail_password = process.env.GMAIL_PASSWORD;
-    var transport = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
-        requireTLS: true,
-        auth: {
-            user: gmail_email,
-            pass: gmail_password,
-        }
-    });
- 
-
-    var mailOptions = {
-        from: "memory-stack",
-        to: email,
-        subject: "Email Confirmation for Account Activation",
-        html: `<h1>Thank You For Choosing Us</h1>
+  var mailOptions = {
+    from: "memory-stack",
+    to: email,
+    subject: "Email Confirmation for Account Activation",
+    html: `<h1>Thank You For Choosing Us</h1>
             <p1>Press <a href=https://api-memory-stack.herokuapp.com/api/verify/${authToken} > here </a> to verify your email.</p1> <br><br>
-            <p1>The Link will expire in <strong>20 minutes!</strong></p1>`
-    };
+            <p1>The Link will expire in <strong>20 minutes!</strong></p1>`,
+  };
 
-    console.log(mailOptions);
+  console.log(mailOptions);
 
-    transport.sendMail(mailOptions, (err, res) => {
-        if (err) {
-            console.log("Error occured!");
-            console.log(err);
-            res.status(500).json({
-                message: err
-            })
-        }
-        else {
-            console.log("Email sent!");
-            res.status(200).json({
-                message: "email sent"
-            })
-        }
-    });
-}
+  transport.sendMail(mailOptions, (err, res) => {
+    if (err) {
+      console.log("Error occured!");
+      console.log(err);
+      res.status(500).json({
+        message: err,
+      });
+    } else {
+      console.log("Email sent!");
+      res.status(200).json({
+        message: "email sent",
+      });
+    }
+  });
+};
 
 module.exports = {
+  userExists: (req, res) => {
+    var username = req.body.username;
 
-    userExists : (req, res) => {
-        var username = req.body.username;
-    
-        User.find({ username: username })
-            .exec()
-            .then(result => {
-                if (result.length) {
-                    console.log("User already exists.");
-                    res.status(200).json({
-                        message: "True"
-                    });
-                }
-                else {
-                    console.log("User doesn't exists.");
-                    res.status(200).json({
-                        message: "False"
-                    });
-                }
-            })
-            .catch(error => {
+    User.find({ username: username })
+      .exec()
+      .then((result) => {
+        if (result.length) {
+          console.log("User already exists.");
+          res.status(200).json({
+            message: "True",
+          });
+        } else {
+          console.log("User doesn't exists.");
+          res.status(200).json({
+            message: "False",
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).json({
+          message: error,
+        });
+      });
+  },
+
+  verifyToken: (req, res) => {
+    const { authToken } = req.params;
+    console.log("hello");
+    if (authToken) {
+      jwt.verify(
+        authToken,
+        process.env.JWT_ACC_ACTIVATE,
+        (err, decodedToken) => {
+          if (err) {
+            return res
+              .status(400)
+              .json({ error: "Incorrect or Expired Link." });
+          }
+          const { email, username } = decodedToken;
+          console.log(email);
+          console.log(username);
+          const user = User.find({ email: email });
+          if (user) {
+            // console.log(user);
+
+            User.update({ username: username }, { $set: { isVerified: true } })
+              .exec()
+              .then((result) => {
+                console.log(result);
+                res.redirect(
+                  "https://memorystack.herokuapp.com/verification-success"
+                );
+              })
+              .catch((error) => {
                 console.log(error);
                 res.status(500).json({
-                    message: error
-                })
-            })
-    },
-
-    verifyToken :  (req, res) => {
-        const { authToken } = req.params;
-        console.log("hello");
-        if (authToken) {
-            jwt.verify(authToken, process.env.JWT_ACC_ACTIVATE, (err, decodedToken) => {
-                if (err) {
-                    return res.status(400).json({ error: 'Incorrect or Expired Link.' });
-                }
-                const { email, username } = decodedToken;
-                console.log(email);
-                console.log(username);
-                const user = User.find({ email: email });
-                if (user) {
-                    // console.log(user);
-    
-                    User.update({ username: username }, { $set: { isVerified: true } })
-                        .exec()
-                        .then(result => {
-                            console.log(result);
-                            res.redirect('https://memorystack.herokuapp.com/verification-success');
-                        })
-                        .catch(error => {
-                            console.log(error);
-                            res.status(500).json({
-                                message: error
-                            })
-                        })
-                    // res.send("<h1>Account Verified!</h1><p1>Please <a href=http://192.168.0.103:3000/api/verification-success > Login </a></p1>");
-    
-                }
-                else {
-                    // res.json('user not found');
-                    res.status(200).json({
-                        message: "user not found"
-                    })
-                }
-            });
-    
-        }
-        else {
-            // return res.json({error : "Something went wrong!"}); 
-            res.status(250).json({
-                message: "Something went wrong!"
-            })
-        }
-    },
-
-    signup: async (req, res) => {
-        var username = req.body.username;
-        var password = req.body.password;
-        var email = req.body.email;
-    
-        const user = new User({
-            username: username,
-            password: password,
-            email: email
-        });
-    
-        console.log(username);
-        console.log(password);
-        console.log(email);
-    
-        //then?
-        user.save().then(result1 => {
-            const authToken = jwt.sign({ email, username }, process.env.JWT_ACC_ACTIVATE)
-            const result = sendEmail(email, authToken);
+                  message: error,
+                });
+              });
+            // res.send("<h1>Account Verified!</h1><p1>Please <a href=http://192.168.0.103:3000/api/verification-success > Login </a></p1>");
+          } else {
+            // res.json('user not found');
             res.status(200).json({
-                message: "user created."
+              message: "user not found",
             });
-        })
-        .catch(error => {
-            console.log(error);
-            res.status(500).json({
-                message: error
-            });
-        })
-    }, 
-
-    login : (req, res) => {
-        var username = req.body.username;
-        var password = req.body.password;
-    
-        User.find({ username: username })
-        .exec()
-        .then(result => {
-            if (result.length) {
-                var email = result[0].email;
-
-                console.log(result);
-                console.log(password);
-                console.log(result[0].password);
-                console.log(email);
-
-                //password match and jwt generation
-                bcrypt.compare(password, result[0].password)
-                    .then(comparedResult => {
-                        const uniqueString = jwt.sign({ email, username }, process.env.JWT_ACC_ACTIVATE1, { expiresIn: '2h' });
-                        console.log(uniqueString);
-                        console.log(comparedResult);
-
-                        if (comparedResult) {
-                            if (!result[0].isVerified) {
-                                res.status(200).json({
-                                    message: "Please vefify your account to login",
-                                    result: "false"
-                                })
-                            }
-                            else {
-                                res.status(200).json({
-                                    message: uniqueString,
-                                    result: "true"
-                                })
-                            }
-                        }
-                        else {
-                            res.status(200).json({
-                                message: "Incorrect username or password",
-                                result: "false"
-                            })
-                        }
-                    })
-                    .catch(error => {
-                        console.log(error);
-                        res.status(500).json({
-                            message: error,
-                        })
-                    })
-
-
-            }
-            else {
-                console.log(username);
-                res.status(200).json({
-                    message: "Incorrect username or password",
-                    result: "false"
-                })
-            }
-        })
-        .catch(error => {
-            console.log(error);
-            res.status(500).json({
-                message: error
-            });
-        });
+          }
+        }
+      );
+    } else {
+      // return res.json({error : "Something went wrong!"});
+      res.status(250).json({
+        message: "Something went wrong!",
+      });
     }
+  },
+
+  signup: async (req, res) => {
+    var username = req.body.username;
+    var password = req.body.password;
+    var email = req.body.email;
+
+    const user = new User({
+      username: username,
+      password: password,
+      email: email,
+    });
+
+    console.log(username);
+    console.log(password);
+    console.log(email);
+
+    //then?
+    user
+      .save()
+      .then((result1) => {
+        const authToken = jwt.sign(
+          { email, username },
+          process.env.JWT_ACC_ACTIVATE
+        );
+        const result = sendEmail(email, authToken);
+        res.status(200).json({
+          message: "user created.",
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).json({
+          message: error,
+        });
+      });
+  },
+
+  login: (req, res) => {
+    var username = req.body.username;
+    var password = req.body.password;
+
+    User.find({ username: username })
+      .exec()
+      .then((result) => {
+        if (result.length) {
+          var email = result[0].email;
+
+          console.log(result);
+          console.log(password);
+          console.log(result[0].password);
+          console.log(email);
+
+          //password match and jwt generation
+          bcrypt
+            .compare(password, result[0].password)
+            .then((comparedResult) => {
+              const uniqueString = jwt.sign(
+                { email, username },
+                process.env.JWT_ACC_ACTIVATE1,
+                { expiresIn: "2h" }
+              );
+              console.log(uniqueString);
+              console.log(comparedResult);
+
+              if (comparedResult) {
+                if (!result[0].isVerified) {
+                  res.status(200).json({
+                    message: "Please vefify your account to login",
+                    result: "false",
+                  });
+                } else {
+                  res.status(200).json({
+                    message: uniqueString,
+                    result: "true",
+                  });
+                }
+              } else {
+                res.status(200).json({
+                  message: "Incorrect username or password",
+                  result: "false",
+                });
+              }
+            })
+            .catch((error) => {
+              console.log(error);
+              res.status(500).json({
+                message: error,
+              });
+            });
+        } else {
+          console.log(username);
+          res.status(200).json({
+            message: "Incorrect username or password",
+            result: "false",
+          });
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        res.status(500).json({
+          message: error,
+        });
+      });
+  },
+
+  getUserDetail: async (req, res) => {
+    try {
+      const username = req.params.username;
+      const userData = await User.findOne({ username: username }).populate(
+        "createdLogs"
+      );
+      if (!userData) {
+        return res.status(404).json({ message: "No such user" });
+      }
+      return res.status(200).json({ user: userData });
+    } catch (error) {
+      return res.status(500).json({ message: error });
+    }
+  },
 };
--- routes/Route.js
+++ routes/Route.js
@@ -1,28 +1,27 @@
-const express = require('express');
-const router = express.Router('router');
-const userController = require('../controller/userController');
-const logController = require('../controller/logController');
-const thoughtController = require('../controller/thoughtController');
-const aboutController = require('../controller/aboutController');
-require('dotenv').config();
+const express = require("express");
+const router = express.Router("router");
+const userController = require("../controller/userController");
+const logController = require("../controller/logController");
+const thoughtController = require("../controller/thoughtController");
+const aboutController = require("../controller/aboutController");
+require("dotenv").config();
 
-
-router.get('/', (req, res)=>{
-    res.status(200).json({
-        message: 'Server is up!'
-    });
+router.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Server is up!",
+  });
 });
 
-
-router.post('/userExists'       , userController.userExists);  //username exists or not
-router.get ('/verify/:authToken', userController.verifyToken);  // verify the auth token
-router.post('/signup'           , userController.signup);
-router.post('/login'            , userController.login);
-router.post('/displayLogs'      , logController.displayLogs);
-router.post('/createLog'        , logController.createLog); //done - handled if no user is matched, if no new loglength is 0
-router.post('/setThought'       , thoughtController.setThought);
-router.post('/thought'          , thoughtController.allthought); //dont upload on github, done - handled if no user is matched
-router.post('/setAbout'         , aboutController.setAbout);
-router.post('/about'            , aboutController.about); //dont upload on github
+router.post("/userExists", userController.userExists); //username exists or not
+router.get("/verify/:authToken", userController.verifyToken); // verify the auth token
+router.post("/signup", userController.signup);
+router.post("/login", userController.login);
+router.post("/displayLogs", logController.displayLogs);
+router.post("/createLog", logController.createLog); //done - handled if no user is matched, if no new loglength is 0
+router.post("/setThought", thoughtController.setThought);
+router.post("/thought", thoughtController.allthought); //dont upload on github, done - handled if no user is matched
+router.post("/setAbout", aboutController.setAbout);
+router.post("/about", aboutController.about); //dont upload on github
+router.get("/user/:username", userController.getUserDetail);
 
 module.exports = router;
