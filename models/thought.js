2206c5c2beee09149c09b1c96533a565c2e7e3d3
himanshumishra508 <mishrahimanshu.hmi@gmail.com>
2021-11-04 01:07:28 +0530

    Added thought model

---
+++ models/thought.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const thoughtSchema = new mongoose.Schema({
+  thought: {
+    type: String,
+    required: true,
+  },
+  creator: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Thought", thoughtSchema);
