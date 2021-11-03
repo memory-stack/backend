09c08cd8a69ea7ec5d41cb1d1419be11f81e78bb
himanshumishra508 <mishrahimanshu.hmi@gmail.com>
2021-11-04 01:10:07 +0530

    Updated  log model

--- models/log.js
+++ models/log.js
@@ -1,17 +1,18 @@
 const mongoose = require("mongoose");
 
-const logSchema = new mongoose.Schema(
-  {
-    logMessage: {
-      type: String,
-      required: true,
-    },
-    creator: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
+const logSchema = new mongoose.Schema({
+  logMessage: {
+    type: String,
+    required: true,
   },
-  { timestamps: true }
-);
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 module.exports = mongoose.model("Log", logSchema);
