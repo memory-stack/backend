88ac14ef087e0ce552481da9ba2a4f4cb03e7ce7
himanshumishra508 <mishrahimanshu.hmi@gmail.com>
2021-11-15 20:17:56 +0530

    Only new log emit on log insertion

--- Server.js
+++ Server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
-    methods: ["GET", "POST"]
+    methods: ["GET", "POST"],
   },
 });
 // socket.set('origins','*:*');
@@ -16,12 +16,12 @@ const io = require("socket.io")(server, {
 
 io.on("connection", async (socket) => {
   console.log("A user connected: ", socket.id);
-  const allLogs = await Log.find().populate('creator','username');
+  const allLogs = await Log.find().populate("creator", "username");
   socket.emit("recentLogs", allLogs);
 
-  Log.watch().on("change", async (chnage) => {
-    const allLogs = await Log.find().populate('creator','username');
-    socket.emit("recentLogs", allLogs);
+  Log.watch().on("change", async (change) => {
+    console.log(change["fullDocument"]);
+    socket.emit("newLog", change["fullDocument"]);
   });
 
   socket.on("disconnect", () => {
