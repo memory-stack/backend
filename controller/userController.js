0cb4a8cd785a8e801526c1dd1c743a0d33d0c696
kkb-10 <kkbajpai.kk@gmail.com>
2021-11-27 01:45:01 +0530

    Verification email colours and structure modified

--- controller/userController.js
+++ controller/userController.js
@@ -132,11 +132,11 @@ const sendEmail = (email, authToken) => {
             
             <body style="background-color: #f4f4f4; margin: 0 !important; padding: 0 !important;">
                 <!-- HIDDEN PREHEADER TEXT -->
-                <div style="display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: 'Lato', Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;"> We're thrilled to have you here! Get ready to dive into your new account. </div>
+                <div style="display: none; font-size: 1px; color: #EE5C54; line-height: 1px; font-family: 'Lato', Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;"> We're thrilled to have you here! Get ready to dive into your new account. </div>
                 <table border="0" cellpadding="0" cellspacing="0" width="100%">
                     <!-- LOGO -->
                     <tr>
-                        <td bgcolor="#FFA73B" align="center">
+                        <td bgcolor="#A772FF" align="center">
                             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
                                 <tr>
                                     <td align="center" valign="top" style="padding: 40px 10px 40px 10px;"> </td>
@@ -145,10 +145,10 @@ const sendEmail = (email, authToken) => {
                         </td>
                     </tr>
                     <tr>
-                        <td bgcolor="#FFA73B" align="center" style="padding: 0px 10px 0px 10px;">
+                        <td bgcolor="#A772FF" align="center" style="padding: 0px 10px 0px 10px;">
                             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
                                 <tr>
-                                    <td bgcolor="#ffffff" align="center" valign="top" style="padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;">
+                                    <td bgcolor="#222222" align="center" valign="top" style="padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #EEEEEE; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;">
                                         <h1 style="font-size: 48px; font-weight: 400; margin: 2;">Welcome!</h1> <img src=" https://img.icons8.com/clouds/100/000000/handshake.png" width="125" height="120" style="display: block; border: 0px;" />
                                     </td>
                                 </tr>
@@ -156,48 +156,48 @@ const sendEmail = (email, authToken) => {
                         </td>
                     </tr>
                     <tr>
-                        <td bgcolor="#f4f4f4" align="center" style="padding: 0px 10px 0px 10px;">
-                            <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
-                                <tr>
-                                    <td bgcolor="#ffffff" align="left" style="padding: 20px 30px 40px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
-                                        <p style="margin: 0;">We're excited to have you get started. First, you need to confirm your account. Just press the button below.</p>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td bgcolor="#ffffff" align="left">
-                                        <table width="100%" border="0" cellspacing="0" cellpadding="0">
-                                            <tr>
-                                                <td bgcolor="#ffffff" align="center" style="padding: 20px 30px 60px 30px;">
-                                                    <table border="0" cellspacing="0" cellpadding="0">
-                                                        <tr>
-                                                            <td align="center" style="border-radius: 3px;" bgcolor="#FFA73B"><a href=https://api-memory-stack.herokuapp.com/api/verify/${authToken} target="_blank" style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid #FFA73B; display: inline-block;">Confirm Account</a></td>
-                                                        </tr>
-                                                    </table>
-                                                </td>
-                                            </tr>
-                                        </table>
-                                    </td>
-                                </tr> <!-- COPY -->
-                                
-                                <tr>
-                                    <td bgcolor="#ffffff" align="left" style="padding: 0px 30px 20px 30px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
-                                        <p style="margin: 0;">If you have any questions, just reply to this email—we're always happy to help out.</p>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td bgcolor="#ffffff" align="left" style="padding: 0px 30px 40px 30px; border-radius: 0px 0px 4px 4px; color: #666666; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
-                                        <p style="margin: 0;">Cheers,<br>MEMORY STACK Team</p>
-                                    </td>
-                                </tr>
-                            </table>
+                        <!-- box-shadow: 0px 0px 20px 4px rgba(0, 0, 0, 0.25); -->
+                        
+                        <td bgcolor="#1A1A1A" align="center" style="padding: 0px 10px 80px 10px;">
+                                <table  border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
+                                    <tr>
+                                        <td bgcolor="#222222" align="left" style="padding: 20px 30px 40px 30px; color: #999999; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                                            <p style="margin: 0;">We're excited to have you get started. First, you need to confirm your account. Just press the button below.</p>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td bgcolor="#222222" align="left">
+                                            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                                <tr>
+                                                    <td bgcolor="#222222" align="center" style=" padding: 20px 30px 60px 30px;">
+                                                        <table border="0" cellspacing="0" cellpadding="0">
+                                                            <tr>
+                                                                <td align="center" style="border-radius: 3px;" bgcolor="#A772FF"><a href=https://api-memory-stack.herokuapp.com/api/verify/${authToken} target="_blank" style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 0px solid #FFA73B; display: inline-block;">Confirm Account</a></td>
+                                                            </tr>
+                                                        </table>
+                                                    </td>
+                                                </tr>
+                                            </table>
+                                        </td>
+                                    </tr> <!-- COPY -->
+                                    
+                                    <tr>
+                                        <td bgcolor="#222222" align="left" style="padding: 0px 30px 20px 30px; color: #999999; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                                            <p style="margin: 0;">If you have any questions, just reply to this email—we're always happy to help out.</p>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td bgcolor="#222222" align="left" style=" padding: 0px 30px 40px 30px; border-radius: 0px 0px 4px 4px; color: #999999; font-family: 'Lato', Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;">
+                                            <p style="margin: 0;">Cheers,<br>MEMORY STACK Team</p>
+                                        </td>
+                                    </tr>
+                                </table>
                         </td>
                     </tr>
                 </table>
             </body>
             </html>`,
   };
-
-  console.log(mailOptions);
 
   transport.sendMail(mailOptions, (err, res) => {
     if (err) {
