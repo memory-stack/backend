6a83e7216dcf650d8464a72456821bcdbf36e183
himanshumishra508 <mishrahimanshu.hmi@gmail.com>
2021-12-11 22:38:01 +0530

    Removed thought controller

--- routes/Route.js
+++ routes/Route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router('router');
 const userController = require('../controller/userController');
 const logController = require('../controller/logController');
-const thoughtController = require('../controller/thoughtController');
+// const thoughtController = require('../controller/thoughtController');
 const aboutController = require('../controller/aboutController');
 require('dotenv').config();
 
@@ -20,8 +20,8 @@ router.post('/signup'           , userController.signup);
 router.post('/login'            , userController.login);
 router.get ('/displayLogs/:username'      , logController.displayLogs);
 router.post('/createLog'        , logController.createLog); //done - handled if no user is matched, if no new loglength is 0
-router.post('/setThought'       , thoughtController.setThought);
-router.get ('/thought/:username'          , thoughtController.allThought); //dont upload on github, done - handled if no user is matched
+// router.post('/setThought'       , thoughtController.setThought);
+// router.get ('/thought/:username'          , thoughtController.allThought); //dont upload on github, done - handled if no user is matched
 router.post('/setAbout'         , aboutController.setAbout);
 router.get ('/about'            , aboutController.about); //dont upload on github
 router.get ('/user/:username'    , userController.getUserDetails);
