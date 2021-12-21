7e24bbd022332d2c7af4c1b1c1b0c875e1ed5f1c
himanshumishra508 <mishrahimanshu.hmi@gmail.com>
2021-12-22 01:23:19 +0530

    Removed sockets for recent logs

--- Server.js
+++ Server.js
@@ -5,31 +5,7 @@ require("dotenv").config();
 const port = process.env.PORT || 3000;
 const app = require("./App.js");
 const server = http.createServer(app);
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-  },
-});
-// socket.set('origins','*:*');
-// const io = new socket.Server(server);
 
-io.on("connection", async (socket) => {
-  console.log("A user connected: ", socket.id);
-  const allLogs = await Log.find().populate("creator", "username");
-  socket.emit("recentLogs", allLogs);
-
-  Log.watch().on("change", async (change) => {
-    const logChange = { ...change["fullDocument"] };
-    const newLog = new Log(logChange);
-    await newLog.populate("creator", "username");
-    socket.emit("newLog", newLog);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("A user disconnected");
-  });
-});
 require("./DB/connection"); // DB connection
 
 server.listen(port, () => console.log(`API running at port ${port}`));
